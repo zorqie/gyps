ede3541133e9e31e8b4eab48faf7e255456c857e
Zorqie <zorqie@yahoo.com>
2018-02-27 15:24:11 -0800

    Styling EventHeader

--- src/ui/event/EventHeader.jsx
+++ src/ui/event/EventHeader.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 
-import { Header } from 'semantic-ui-react'
+import { Header, Label } from 'semantic-ui-react'
 
 import GigTimespan from '../GigTimespan.jsx'
+import styles from '../styles'
 
-export default function EventHeader({event}) {
+export default function EventHeader({event, showVenue = true}) {
 	return (
-		<Header style={{fontWeight: '300'}}> 
-			<b style={{fontSize: 'x-large'}}>{event.name}</b>
+		<Header dividing> 
+			<b style={{fontWeight: '300',fontSize: 'x-large'}}>{event.name}</b>
 			{event.acts.length 
 				&& <span> (with {event.acts.map(act => act.name).join(', ')})</span>
 				|| ''
 			}
-			<div>
+			{showVenue 
+				&& <Header floated='right'>
+					<Label style={styles.venue} basic>
+						<span style={{fontSize:'x-small'}}>at</span>
+						<Label.Detail>{event.venue.name}</Label.Detail>
+					</Label>
+				</Header>
+				|| ''
+			}
+			<Header.Subheader floated='left'>
 				<GigTimespan gig={event} showRelative={true} style={{fontSize: 'smaller'}} />
-			</div>
-			<div style={{fontWeight: '300'}}>at <b>{event.venue.name}</b></div>
+			</Header.Subheader>
 	    </Header>
 	)
 }
